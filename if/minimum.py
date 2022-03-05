63e3b92effd18c4aed870496b5d3c447ad33e6e9
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-05 19:48:34 +0530

    Create minimum.py

---
+++ if/minimum.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the first number"))
+y=int(input("Enter the second number"))
+if (x>y):
+  print(f'{x} is greater')
+elif(x<y):
+  print(f'{y} is greater')
+else:
+  print("Both numbers are equal")
+  
+  
