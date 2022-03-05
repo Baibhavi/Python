20214fb14b309724dc65d08cbbbd20c7f8a23c01
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-05 11:52:53 +0530

    Create area.py

---
+++ area.py
@@ -0,0 +1,6 @@
+x=float(input("Enter the length of the rectangle"))
+y=float(input("Enter the breadth of the rectangle"))
+area=x*y
+peri=2*(x+y)
+print(f'The Area of rectangle is {area}')
+print(f'The Perimeter of the rectangle is {peri}')
