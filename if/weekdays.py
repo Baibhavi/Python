0459e1438784b79ce7762e285b23283eae813acf
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-06 21:14:31 +0530

    Create weekdays.py

---
+++ if/weekdays.py
@@ -0,0 +1,19 @@
+#Write a program to input week number and print week day.
+x=str(input("Enter week number\n"))
+if( x==1):
+  print("Sunday")
+elif(x==2):
+  print("Monday")
+elif(x==3):
+  print("Tuesday")
+elif(x==4):
+  print("Wednesday")
+elif(x==5):
+  print("Thursday")
+elif(x==6):
+  print("Friday")
+elif(x==7):
+  print("Saturday") 
+else:
+  print("You entered invalid week number")
+  
