40fae45c1397b6c4e8d5713909b281bc0c2508e6
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-06 12:03:02 +0530

    Create wholenum.py

---
+++ if/wholenum.py
@@ -0,0 +1,12 @@
+x=int(input("Enter the number to be checked\n"))
+if(x>0):
+  print("Positve")
+elif(x<0):
+  print("Negative")
+else :
+  print("Zero")
+            
+            
+            
+            
+            '
