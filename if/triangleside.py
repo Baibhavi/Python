12f2224bd93d61d7e991f9b73eb4110b76b1da97
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-06 22:30:01 +0530

    Update triangleside.py

--- if/triangleside.py
+++ if/triangleside.py
@@ -2,7 +2,7 @@
 x=float(input("Enter the length of first side\n"))
 y=float(input("Enter the length of second side\n"))
 z=float(input("Enter the length of third side\n"))
-if(x+y>z or x+z>y or y+z>x):
+if(x+y>z and x+z>y and y+z>x):
    print("The triangle is valid.")
 else:
    print("The triangle is invalid")
