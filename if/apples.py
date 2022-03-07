0f178831f290011db49e891dc335eaf350d7a16f
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-07 20:54:55 +0530

    Update apples.py

--- if/apples.py
+++ if/apples.py
@@ -5,4 +5,9 @@
 	If he picks them in a group of 4, 1 apple is left behind.
 	If he picks them in a group of 3, 1 apple is left behind.
 	If he picks them in a group of 2, 1 apple is left behind.'''
-x
+x=int(input("Enter the number of apples"))
+if(x%7==0 and x%6==1 and x%5==1 and x%4==1 x%3==1 x%3==1 x%2==1):
+	print("The number apples are valid")
+else:
+	print("The number of apples are invalid")
+	
