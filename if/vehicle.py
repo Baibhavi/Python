e07526f8700483d559130e967c6ff5c87aa6bb14
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-07 20:49:54 +0530

    Create vehicle.py

---
+++ if/vehicle.py
@@ -0,0 +1,14 @@
+'''An automobile company manufactures both a two wheeler (TW) and a four wheeler (FW). A company manager wants to make the production of both types of vehicle according to 
+the given data below:
+•	1st data, Total number of vehicle (two-wheeler + four-wheeler)=v
+•	2nd data, Total number of wheels = W
+The task is to find how many two-wheelers as well as four-wheelers need to manufacture as per the given data.'''
+
+v=int(input("Enter the total number of vehicles"))
+w=int(input("Enter the total number of wheels"))
+tw=v-y
+fw=w-v
+print("Number of two wheelers = ",tw)
+print("Number of four wheelers = ",fw)
+
+
