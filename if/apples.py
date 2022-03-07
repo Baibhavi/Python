730229fc93597ce8d2ca65f8c61aa3221268ac4c
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-07 19:55:30 +0530

    Create apples.py

---
+++ if/apples.py
@@ -0,0 +1,8 @@
+'''A man has certain number of apples.
+	If he picks them in a group of 7, he can pick all of them.
+	If he picks them in a group of 6, 1 apple is left behind.
+	If he picks them in a group of 5, 1 apple is left behind.
+	If he picks them in a group of 4, 1 apple is left behind.
+	If he picks them in a group of 3, 1 apple is left behind.
+	If he picks them in a group of 2, 1 apple is left behind.'''
+x
