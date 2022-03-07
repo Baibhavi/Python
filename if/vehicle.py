3bf2048ef905d588b88946834ed3c499e97ed9a1
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-03-07 21:01:09 +0530

    Update vehicle.py

--- if/vehicle.py
+++ if/vehicle.py
@@ -1,14 +1,20 @@
-'''An automobile company manufactures both a two wheeler (TW) and a four wheeler (FW). A company manager wants to make the production of both types of vehicle according to 
-the given data below:
+'''An automobile company manufactures both a two wheeler (TW) and a four wheeler (FW). A company manager wants to make the production of both types of vehicle according to the given data below:
 •	1st data, Total number of vehicle (two-wheeler + four-wheeler)=v
 •	2nd data, Total number of wheels = W
-The task is to find how many two-wheelers as well as four-wheelers need to manufacture as per the given data.'''
+The task is to find how many two-wheelers as well as four-wheelers need to manufacture as per the given data.
+Example :
+Input :
+•	200  -> Value of V
+•	540   -> Value of W
+Output :
+•	TW =130 FW=70
+'''
 
 v=int(input("Enter the total number of vehicles"))
 w=int(input("Enter the total number of wheels"))
-tw=v-y
-fw=w-v
-print("Number of two wheelers = ",tw)
-print("Number of four wheelers = ",fw)
+y=w/2 - v
+x=v-y
+print("Number of two wheelers = ",x)
+print("Number of four wheelers = ",y)
 
 
