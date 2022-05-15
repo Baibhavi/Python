2c5a71a35f903f358c0a810d9f9638ad61571cbe
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-05-15 20:10:19 +0530

    Create candy.py

---
+++ loops/candy.py
@@ -0,0 +1,12 @@
+'''There are N kids in a play Group. The kid with number K (1 ≤ K ≤ N) will be happy if he receives at 
+least AK candies. There is C candies in all. 
+The school staff is interested in knowing whether it is possible to make all the N kids happy by 
+giving each kid at least as many candies as he wants, that is, the Kth kid should receive at least AK 
+candies. Each candy can be given to only one kid. Print Yes if it is possible and No otherwise. 
+Input 
+Input N, C and N integers A1, A2, ..., AN. 
+Output 
+Yes if it possible to make all kids happy and No otherwise.'''
+n=int(input("Enter the number of kids"))
+c=int(input("Enter the number of candies"))
+an=int(input("
