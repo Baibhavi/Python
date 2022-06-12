01efb9ac53d13365836c84a1db291c6f9c5afdc0
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-06-12 16:08:44 +0530

    Add files via upload

---
+++ loops/prime_in_interval.py
@@ -0,0 +1,24 @@
+'''Print all Prime Numbers in an Interval'''
+a=int(input("Enter the number from which you want to check prime numbers: "))
+b=int(input("Enter the number upto which you want to check prime numbers: "))
+for num in range (a,b+1):
+    if num>1 :
+        for i in range (2,num):
+            if num%i==0:
+                break
+        else :
+                print(num)
+
+'''lower = 900
+upper = 1000
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)    '''            
