1f02d83c5621c6524ed852671acd96c41e783b2e
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-06-18 21:20:02 +0530

    Add files via upload

---
+++ Strings/Manipulations/len_string.py
@@ -0,0 +1,11 @@
+'''Find the Length of a String
+'''
+a=input("Enter a string")
+print("The length of the string is:",len(a))
+
+#using loop
+a=input("Enter a string")
+count=0
+for i in a:
+    count+=1
+print("Length of string:",count)    
