010d3df7476cb64cca8a479b3e9776259b57c735
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-06-26 18:34:20 +0530

    Add files via upload

---
+++ Tuples/row_insertion.py
@@ -0,0 +1,7 @@
+'''List 1's first row and list2's last row insert in list 3'''
+list1=[(1,2),(3,4),(5,6)]
+list2=[(7,8),(9,10),(11,12)]
+lst=[]
+lst.append(list1[0])
+lst.append(list2[2])
+print(lst)        
