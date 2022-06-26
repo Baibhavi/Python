882fd84d002c4872c64e4f7f97cefa3c6789bd07
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-06-26 18:40:15 +0530

    Add files via upload

---
+++ Tuples/ele_row_insertion.py
@@ -0,0 +1,7 @@
+'''List1's first row first element and list2's last row last element in list 3.'''
+list1=[(1,2),(3,4),(5,6)]
+list2=[(7,8),(9,10),(11,12)]
+list3=[]
+list3.append(list1[0][0])
+list3.append(list2[2][1])
+print(list3)
