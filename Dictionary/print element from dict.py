555aa1d7184e6044e2f11ffa792fb3cfdef638d7
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-06-28 20:35:40 +0530

    Add files via upload

---
+++ Dictionary/print element from dict.py
@@ -0,0 +1,13 @@
+'''WAP to print different elements of a dictionary'''
+
+d={1:2,3:4,5:6,7:8,9:10}
+print(d)
+# To print keys only
+k=d.keys()
+print(k)
+# To print values only
+v=d.values()
+print(v)
+# To print items of dictionary/ converting into tuple
+i=d.items()
+print(i)
