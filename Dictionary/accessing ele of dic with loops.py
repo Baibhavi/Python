28f1a6200db9142bf75c11c3604ff0034ab12575
Baibhavi Pandey <100146898+Baibhavi@users.noreply.github.com>
2022-06-28 20:58:53 +0530

    Add files via upload

---
+++ Dictionary/accessing ele of dic with loops.py
@@ -0,0 +1,8 @@
+'''WAP to access elements of dictionary using loops'''
+x={1:2,3:4,5:6,7:8}
+#to access keys
+for i in x:
+    print("key = ",i)
+#to access values
+for i in x:
+    print("value = ",x[i])    
